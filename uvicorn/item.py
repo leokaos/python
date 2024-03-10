82a6d7e2270d44c0e2d2120217058d3183726082
Leonardo Guerra <leokaos@gmail.com>
2024-03-10 18:39:39 +0000

    reset

--- uvicorn/item.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-
-class Item(BaseModel):
-    id: str = None
-    title: str
-    timestamp: datetime
-    description: str | None = None
